2e0d01eacbcf50bb2678cdafd93baa191d063413
Javier López <jilopez8@uc.cl>
2017-11-04 03:24:24 -0300

    Fix table buttons top margins

--- src/app/order-form/order-form.component.ts
+++ src/app/order-form/order-form.component.ts
@@ -15,13 +15,13 @@ export class OrderFormComponent implements OnInit {
   address = '';
   success = null;
   msg = '';
-  token :any;
+  token: any;
   products: any = [];
-  page = 1;    
+  page = 1;
   drugsCategories: any = [];
-  hide: boolean = true;
+  hide = true;
 
-  constructor( private router: Router, private user: UserService, private api: ApiService) {
+  constructor(private router: Router, private user: UserService, private api: ApiService) {
   }
 
   ngOnInit() {
@@ -40,110 +40,94 @@ export class OrderFormComponent implements OnInit {
     };
     this.token = localStorage.getItem('token');
     this.api.postData(`/transaction`, body, true, this.token)
-    .then(data => {
-      if (data['success']) {
-        this.success = true;
-        this.msg = 'La compra se ha realizado con éxito.';
-      } else {
-        this.success = false;
-        this.msg = data['msg'];
-      }
-    });
+      .then(data => {
+        if (data['success']) {
+          this.success = true;
+          this.msg = 'La compra se ha realizado con éxito.';
+        } else {
+          this.success = false;
+          this.msg = data['msg'];
+        }
+      });
   }
 
+  getCategories() {
+    // Request categories
+    let categories = [];
+    let page = 1;
+    let categoriesLeft = true;
+    this.token = localStorage.getItem('token');
+
+    this.api.getDataCustom(`categories`, true, this.token)
+      .then(data => {
+        this.drugsCategories = data;
+        this.drugsCategories = this.drugsCategories
+          .filter(category => category.context === "MEDICAMENTOS")
+          .map(category => category.id);
+        return this.drugsCategories;
+      }).then(x => this.getProducts());
+  }
 
   getProducts() {
     // Request products
     this.token = localStorage.getItem('token');
     this.api.getData(`/products?page=${this.page}`, true, this.token)
-    .then(data => {      
-      if (data[0]) {
-        this.products = data;
-        this.products.map(product => product.amount = 0);
-        this.products = this.products
-          .filter(product => !this.drugsCategories.includes(product.category));
-           
-            
-      } else {
-        this.page--;
-        this.getProducts();
-      }
-    });
+      .then(data => {
+        if (data[0]) {
+          this.products = data;
+          this.products.map(product => product.amount = 0);
+          this.products = this.products
+            .filter(product => !this.drugsCategories.includes(product.category));
+        } else {
+          this.page--;
+          this.getProducts();
+        }
+      });
   }
-
-  getCategories() {
-    // Request categories
-    let categories = []
-    let page = 1;
-    let categoriesLeft = true;
-    this.token = localStorage.getItem('token');
-    
-    this.api.getDataCustom(`categories`, true, this.token)
-    .then(data => {      
-      
-      this.drugsCategories = data;
-      this.drugsCategories = this.drugsCategories
-        .filter( product => product.context == "MEDICAMENTOS")
-        .map(product => product.id);
-
-        return this.drugsCategories;
-      
-    }).then(x => this.getProducts());
-
-    
-  }
-  //}
 
   nextPage() {
     this.page++;
     this.getProducts();
-    
   }
 
   previousPage() {
-    if(this.page > 1) {
+    if (this.page > 1) {
       this.page--;
       this.getProducts();
-    }   
-    
+    }
   }
   addProductToCart(product) {
     if (product.amount > 0) {
-
       // this makes a copy of the object, otherwise if we add product i'll be a reference
       let p = JSON.parse(JSON.stringify(product));
-      
 
       for (let item of this.api.shoppingCart) {
-        
-        if (p.id == item.id) {
+        if (p.id === item.id) {
           item.amount += p.amount;
           product.amount = 0;
-          this.api.sum += +p.price * +p.amount; 
+          this.api.sum += +p.price * +p.amount;
           return;
         }
       }
-            
+
       this.api.shoppingCart.push(p);
       product.amount = 0;
       // + is unary cast operator
-      this.api.sum += +p.price * +p.amount;  
-         
-      
+      this.api.sum += +p.price * +p.amount;
+
     }
   }
 
   add(product) {
-    product.amount ++;
+    product.amount++;
     this.hide = true;
-
   }
   remove(product) {
     if (product.amount > 0) {
-      product.amount --;
+      product.amount--;
     }
-
   }
+
   checkout() {
     if (this.api.shoppingCart.length > 0) {
       this.router.navigate(['checkout']);
@@ -151,7 +135,6 @@ export class OrderFormComponent implements OnInit {
     else {
       this.hide = false;
     }
-
   }
 
   emptyCart() {
